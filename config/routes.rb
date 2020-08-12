6580d953129d4e104ac2887365e42efd829b6916
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-08-12 15:03:28 +0900

    third commit [add]編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
   get "/" => "home#top"
   get "posts/:id" => "posts#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
